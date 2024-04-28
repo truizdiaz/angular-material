bd2517b45ca3aba9d85d1505b3dfcc9ec1956eea
Tomas Ruiz Diaz <external.diaz_tomas-ruiz@allianzdirect.de>
2024-04-28 11:48:08 +0200

    tables

--- src/app/components/dashboard/users/users.component.ts
+++ src/app/components/dashboard/users/users.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+
+// Angular Material
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { User } from '../../../interfaces/user';
+
+const listUser: User[] = [
+  { userName: 'jperez', firstName: 'Juan', lastName: 'Perez', sex: 'Male' },
+  { userName: 'tmartinez', firstName: 'Tomas', lastName: 'Martinez', sex: 'Male' },
+  { userName: 'agarcia', firstName: 'Alejandro', lastName: 'Garcia', sex: 'Male' },
+  { userName: 'jrios', firstName: 'Julieta', lastName: 'Rios', sex: 'Female' },
+  { userName: 'mprandi', firstName: 'Mariana', lastName: 'Prandi', sex: 'Male' },
+  { userName: 'sblanda', firstName: 'Sofia', lastName: 'Blanda', sex: 'Female' },
+];
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatTableModule, MatIconModule, MatTooltipModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+  displayedColumns: string[] = ['username', 'firstName', 'lastName', 'sex', 'accions'];
+  dataSource = listUser;
 
 }
